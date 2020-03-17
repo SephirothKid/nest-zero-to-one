42908a81e2b72a4cec3b8f8450d84b9d2d177b45
Kid <310388758@qq.com>
2020-03-17 16:31:42 +0800

    Feat: First commit.

---
+++ config/db.ts
@@ -0,0 +1,34 @@
+/*
+ * @Author: Sephiroth·D·Kid
+ * @Date: 2020-03-17 16:14:15
+ * @LastEditors: Sephiroth·D·Kid
+ * @LastEditTime: 2020-03-17 16:29:23
+ * @Description: 数据库配置文件
+ */
+
+const productConfig = {
+  mysql: {
+    port: '数据库端口',
+    host: '数据库地址',
+    user: '用户名',
+    password: '密码',
+    database: '表名',
+    connectionLimit: 10, // 连接限制
+  },
+};
+
+const localConfig = {
+  mysql: {
+    port: '数据库端口',
+    host: '数据库地址',
+    user: '用户名',
+    password: '密码',
+    database: '表名',
+    connectionLimit: 10, // 连接限制
+  },
+};
+
+// 本地开发环境是没有 process.env.NODE_ENV 的，借此来区分[开发环境]和[生产环境]
+const config = process.env.NODE_ENV ? productConfig : localConfig;
+
+export default config;
